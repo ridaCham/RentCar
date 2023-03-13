14db5d3f0c6e166f481b1dcab858e52743359a85
ridaCham <77482726+ridaCham@users.noreply.github.com>
2023-03-13 11:47:53 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entity.Concrete;
+
+Console.WriteLine("Hello, World!");
+CarManager manager = new CarManager(new CarDal());
+
+foreach (Car car in manager.GetAll())
+{
+    Console.WriteLine(car.Description);
+
+}
+
+Console.WriteLine("Hello, World!");
+
